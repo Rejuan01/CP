498c8ed31259933d12ba888182da7d6b29acd50f
Rejuan <rejuan379@gmail.com>
2025-10-11 16:30:29 +0600

    ATcoder DP contest problem

--- F_LCS.cpp
+++ F_LCS.cpp
@@ -16,30 +16,32 @@ using pr = pair<ll, ll>;
 #define yes cout<<"YES"<<endl
 #define no cout<<"NO"<<endl
 void solve(){
-    ll n, x;
-    cin >> n >> x;
-    vi w(n+1), v(n+1);
-    for(ll i = 1; i <= n; i++) cin >> w[i] >> v[i];
-    vector<vi> dp(n+1, vi(1e5+1, 1e18));            // dp[i][j] = min weight needed to get j profit from first i elements
-
-    ll p = v[1];
+    string s1, s2;
+    cin >> s1 >> s2;
+    ll n = s1.size(), m = s2.size();
+    vector<vi> dp(n+1, vi(m+1));     // dp[i][j] = LCS len for first i char of s1 and j car of s2
     for(ll i = 1; i <= n; i++){
-        dp[i][v[i]] = w[i];     // take i th element itself
-        p += v[i];
-        for(ll j = 1; j <= p; j++){
-            dp[i][j] = min(dp[i][j], dp[i-1][j]); 
-            if(v[i] <= j)
-                dp[i][j] = min(dp[i][j], w[i]+dp[i-1][j-v[i]]);
+        for(ll j = 1; j <= m; j++){
+            if(s1[i-1] == s2[j-1]) dp[i][j] = 1+dp[i-1][j-1];
+            else dp[i][j] = max(dp[i-1][j], dp[i][j-1]);
         }
     }
-    for(ll i = 1e5; i >= 0; i--){ // Max profit within knapsack weight
-        if(dp[n][i] <= x){
-            cout << i << endl;
-            return;
+    ll x = dp[n][m];
+    ll i = n, j = m;
+    string ans = "";
+    while(i > 0 && j > 0){      // backtrack
+        if(s1[i-1] == s2[j-1]){
+            ans.pb(s1[i-1]);
+            i--;
+            j--;
         }
+        else if(dp[i-1][j] > dp[i][j-1]) i--;
+        else j--;
     }
+    rev(ans);
+    cout << ans << endl;
+    
 }
-
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);

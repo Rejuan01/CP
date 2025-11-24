3b4ba28d4373fde8cb0f6ad12b674c424bbe6366
Rejuan <rejuan379@gmail.com>
2025-11-24 23:18:14 +0600

    Recursion from ykw problemset

---
+++ W_Reach_Value.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define ll long long
+#define ull unsigned long long
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+bool isReachable(ll n){
+    if(n == 1) return true;
+    if(n%10 == 0 && isReachable(n/10)) return true;
+    else if(n%20 == 0 && isReachable(n/20)) return true;
+    else return false;
+}
+void solve(){
+    ll n;
+    cin >> n;
+    if(isReachable(n)) yes;
+    else no;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}

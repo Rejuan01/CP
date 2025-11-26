26136ade8c8f22114c9740db932491c05d5619a7
Rejuan <rejuan379@gmail.com>
2025-11-26 13:09:21 +0600

    Divisors problems from ykw

---
+++ How_Many_Divisors.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define ll long long
+#define ull unsigned long long
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+void solve(){
+    ll a, b, c;
+    cin >> a >> b >> c;
+    ll ans = 0;
+    for(ll i = a; i <= b; i++){
+        if(c%i == 0) ans++;
+    }
+    cout << ans << endl;
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}

aa324f12513a7377783a02559ba1f50c6be3291c
Rejuan <rejuan379@gmail.com>
2025-12-27 14:14:07 +0600

    Codechef Starters 218

---
+++ Collatz_Conjecture_Hard.cpp
@@ -0,0 +1,50 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"Yes"<<endl
+#define no cout<<"No"<<endl
+void solve(){
+    ll n;
+    cin >> n;
+    ll cnt = 0;
+    if (n == 2 || n == 3) cnt++;
+    else if(n == 6) cnt += 2;
+    if(n == 1 || cnt){
+        cout << cnt << endl; return;
+    }
+    while(n%4 == 0){
+        while(n%8 == 0){
+            n >>= 1;
+            cnt++;
+        }
+        if(n == 4){cnt += 2; break;}
+        if(n == 12){cnt += 3; break;}
+        n ^= 3;n++; // n += 4
+        cnt++;
+    }
+    if(cnt == 0) --cnt;
+    cout << cnt << endl;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}
---
+++ Counting_Optimal_Arrays.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+const ll mod = 998244353;
+void solve(){
+    ll n, m;
+    cin >> n >> m;
+    if(n == 1){
+        cout << m+1 << endl;
+        return;
+    }
+    ll c = 1, x = m;
+    while(x >>= 1) c++;
+    ll k = (1<<c)-1;
+    x = m;
+    ll y = k-m;
+    ll ans = (x-y+1);
+    ans %= mod;
+    cout << ans << endl;
+    
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}

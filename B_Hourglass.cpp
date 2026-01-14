2f67ec938d92c214621ca731399cb7bc1b752395
Rejuan <rejuan379@gmail.com>
2026-01-14 12:48:38 +0600

    CF round 1072

---
+++ B_Hourglass.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+void solve(){
+    ll s, k, m;
+    cin >> s >> k >> m;
+    if(s <= k){
+        ll c = m%k;
+        if(c > s) cout << 0 << endl;
+        else cout << s-c << endl;
+        return;
+    }
+    ll x = s;
+    ll c = m%k, cnt = m/k;
+    if(cnt&1) x = k;
+    if(c > x) cout << 0 << endl;
+    else cout << x-c << endl;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    cin >> t;
+    while(t--){
+       solve();
+    }
+    return 0;
+}

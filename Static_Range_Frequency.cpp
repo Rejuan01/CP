2706c1b89cdbd9bde5ba8d0708272e7000a0b032
Rejuan <rejuan379@gmail.com>
2026-02-06 23:43:28 +0600

    Binary search from ykw

---
+++ Static_Range_Frequency.cpp
@@ -0,0 +1,44 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, greater_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define ll long long
+#define ull unsigned long long
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+void solve(ll tc){
+    ll n, q;
+    cin >> n >> q;
+    vi a(n);
+    map<ll, vi> mp;
+    for(ll i = 0; i < n; i++){
+        cin >> a[i];
+        mp[a[i]].pb(i);
+    }
+    while(q--){
+        ll l, r, x;
+        cin >> l >> r >> x;
+        cout << lower_bound(mp[x].begin(), mp[x].end(), r) - lower_bound(mp[x].begin(), mp[x].end(), l) << endl;
+    }   
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    int t = 1;
+    // cin >> t;
+    for(ll i = 1; i <= t; i++){
+        // cout << "Case " << i << ": ";
+        solve(i);
+    }
+    return 0;
+}

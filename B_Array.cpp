a8ae577477657f4a896793f9d972c8c85f616e57
Rejuan <rejuan379@gmail.com>
2026-04-14 19:56:59 +0600

    CF round 1087

---
+++ B_Array.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, greater_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define ll long long
+#define ull unsigned long long
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+void solve(ll tc){
+    ll n;
+    cin >> n;
+    vi a(n);
+    for(ll i = 0; i < n; i++) cin >> a[i];
+    for(ll i = 0; i < n; i++){
+        ll x = 0, y = 0;
+        for(ll j = i+1; j < n; j++){
+            if(a[j] > a[i]) x++;
+            else if(a[j] < a[i]) y++;
+        }
+        cout << max(x, y) << " ";
+    } cout << endl;
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
+    cin >> t;
+    for(ll i = 1; i <= t; i++){
+        // cout << "Case " << i << ": ";
+        solve(i);
+    }
+    return 0;
+}

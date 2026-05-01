ad0122f1d1947af79c43a71b06219203063c3fd7
Rejuan <rejuan379@gmail.com>
2026-05-02 01:07:10 +0600

    CF round 1094

---
+++ B_Artistic_Balance_Tree.cpp
@@ -0,0 +1,74 @@
+#include<bits/stdc++.h>
+// #include<ext/pb_ds/assoc_container.hpp>
+// #include<ext/pb_ds/tree_policy.hpp>
+using namespace std;
+// using namespace __gnu_pbds;
+// typedef tree<int, null_type, greater_equal<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
+#define ll long long
+#define ull unsigned long long
+using vi = vector<ll>;
+using vp = vector<pair<ll, ll>>;
+using pr = pair<ll, ll>;
+#define pb push_back
+#define rsort(a) sort(a.rbegin(), a.rend())
+#define ssort(a) sort(a.begin(), a.end())
+#define rev(a) reverse(a.begin(), a.end())
+#define yes cout<<"YES"<<endl
+#define no cout<<"NO"<<endl
+void solve(ll tc){
+    ll n, m;
+    cin >> n >> m;
+    vi a(n), b(m);
+    ll s = 0;
+    vi p, q;
+    for(ll i = 0; i < n; i++){
+        cin >> a[i];
+        s += a[i];
+        if(i&1) q.pb(a[i]);
+        else p.pb(a[i]);
+    }
+    ll x = 0, y = 0;
+    for(ll i = 0; i < m; i++){
+        cin >> b[i];
+        if(b[i]&1) x++;
+        else y++;
+    }
+    rsort(p);
+    rsort(q);
+    // for(auto it: p) cout << it << " ";
+    // cout << endl;
+    // for(auto it: q) cout << it << " "; 
+    // cout << endl;
+    ll k = 0;
+    for(ll i = 0; i < p.size() && i < x; i++){
+        if(p[i] >= 0) k += p[i];
+        else{
+            if(i == 0) k += p[i];
+            break;
+        }
+    }
+    // cout << k << endl;
+    for(ll i = 0; i < q.size() && i < y; i++){
+        if(q[i] >= 0) k += q[i];
+        else {
+            if(i == 0) k += q[i];
+            break;
+        }
+    }
+    cout << s-k << endl;
+
+}
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    // freopen("input.txt", "r", stdin);
+    // freopen("output.txt", "w", stdout);
+    int t = 1;
+    cin >> t;
+    for(ll i = 1; i <= t; i++){
+        // cout << "Case " << i << ": ";
+        solve(i);
+    }
+    return 0;
+}
